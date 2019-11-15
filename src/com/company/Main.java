46f433f717da138c8d5eb24f4803ecb9c7d13b4b
Андрей Подшиваленко <1249163@belsu.onmicrosoft.com>
2019-11-15 16:54:47 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("asdfa");
+        System.out.println("asdfa");
+        System.out.println("asdfa");
+        System.out.println("asdfa");
+        System.out.println("asdfa");
+        System.out.println("asdfa");
+        System.out.println("asdfa");
+        Scanner input = new Scanner(System.in);
+        String inputText = input.nextLine();
+        System.out.println(inputText);
+    }
+
+}
